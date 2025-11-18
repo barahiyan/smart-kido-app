631f5397cf9ced95619f823ab6738fe97ae50dc0
barahiyan <97247031+barahiyan@users.noreply.github.com>
2025-11-18 20:14:33 +0300

    Update icons.tsx

--- utils/icons.tsx
+++ utils/icons.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 export const DashboardIcon = ({ className = 'w-6 h-6' }: { className?: string }) => (
@@ -151,5 +152,12 @@ export const SendIcon = ({ className = "w-5 h-5" }) => (
 export const DownloadIcon = ({ className = 'w-5 h-5' }: { className?: string }) => (
     <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
         <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+    </svg>
+);
+
+export const ContactsIcon = ({ className = 'w-5 h-5' }: { className?: string }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M15 21a6 6 0 00-9-5.197m0 0A5.975 5.975 0 0112 13a5.975 5.975 0 01-3 5.197z" />
+        <path strokeLinecap="round" strokeLinejoin="round" d="M19 10l2 2l-2 2" />
     </svg>
 );
