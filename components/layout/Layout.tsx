866963ea7c31ca729e341d2d61feb67679827666
Abdulrahim Barahiyan <abdulrahimbarahiyan@gmail.com>
2025-12-01 19:34:32 +0300

    FINAL PWA AND MOBILE DISPLAY FIX

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -17,10 +17,10 @@ const Layout: React.FC<LayoutProps> = ({ children, page, setPage, user, onLogout
 
   return (
     <div className="flex h-screen bg-light">
-      <Sidebar 
-        page={page} 
-        setPage={setPage} 
-        user={user} 
+      <Sidebar
+        page={page}
+        setPage={setPage}
+        user={user}
         onLogout={onLogout}
         isOpen={isSidebarOpen}
         setIsOpen={setIsSidebarOpen}
@@ -28,7 +28,7 @@ const Layout: React.FC<LayoutProps> = ({ children, page, setPage, user, onLogout
       <div className="flex-1 flex flex-col overflow-hidden lg:ml-64">
         <Header user={user} onLogout={onLogout} toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-light">
-          <div className="container mx-auto px-6 py-8">
+          <div className="w-full max-w-full mx-auto px-4 sm:px-6 py-8">
             {children}
           </div>
         </main>
